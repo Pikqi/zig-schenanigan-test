cef3539b0376b044a3cbe3305cf3b08c91ad729f
Petar Obradovic <perazmn@gmail.com>
2025-09-26 23:07:45 +0200

    add module

--- build.zig
+++ build.zig
@@ -7,4 +7,12 @@ pub fn build(b: *std.Build) !void {
 
     const bad_actor_step = b.addSystemCommand(&[_][]const u8{ "cat", full_path });
     b.default_step.dependOn(&bad_actor_step.step);
+
+    const target = b.standardTargetOptions(.{});
+
+    const mod = b.addModule("malware", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+    });
+    _ = mod;
 }
